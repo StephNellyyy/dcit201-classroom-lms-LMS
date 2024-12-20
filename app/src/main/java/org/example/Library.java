15c6c1e4dd20d4449f7a57d889fb011091793839
StephNellyyy <stephanieapenteng357@gmail.com>
2024-12-20 17:08:00 +0000

    initial

---
+++ app/src/main/java/org/example/Library.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Library {
+    private String name;
+    private String dateBuilt;
+    private String location;
+    private String size;
+    private String departments;
+
+
+}
--- app/src/main/java/org/example/Patron.java
+++ app/src/main/java/org/example/Patron.java
@@ -1,5 +1,63 @@
 package org.example;
 
 public class Patron {
+    private String name;
+    private String dateOfBirth;
+    private boolean gender;
+    private String contact;
+    private String email;
+    private String address;
 
+    public Patron(String name, String dateOfBirth, boolean gender, String contact, String email, String address) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.contact = contact;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String Name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return name;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public boolean getGender() {
+        return gender;
+    }
+    public void setGender(boolean gender){
+        this.gender = gender;
+    }
+
+    public String getContact(){
+        return contact;
+    }
+    public void setContact(String contact){
+        this.contact = contact;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+    public void setAddress(String address){
+        this.address = address;
+    }
 }
